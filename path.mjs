2ec3f5f24544e70385638bab6cd7323a4495cba3
Dimas-WA <dimaswisnuaryadi@gmail.com>
2022-11-22 15:19:57 +0700

    lib path node

---
+++ path.mjs
@@ -0,0 +1,7 @@
+import path from "path";
+
+const file = "C:/Users/dimas.aryadi/Documents/test_upload_dokel.csv";
+
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
