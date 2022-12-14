ff9a14589d1c498899ec3562ba273585f4b61491
Dimas-WA <dimaswisnuaryadi@gmail.com>
2022-12-14 12:10:13 +0700

    update package

--- belajar-nodejs-npm/package.js
+++ belajar-nodejs-npm/package.js
@@ -1,5 +1,10 @@
 import { sayHello, sum } from "belajar-nodejs-npm-library-vandimz";
+import { min, max } from "belajar-nodejs-npm-library-vandimz/number";
 
 console.info(sayHello("dimas"));
 
 console.info(sum([10,2,3,4,5]));
+
+console.info(min(10,20));
+
+console.info(max(10,20));
